9ba709f99945f7880d91daaf7f283e0a05d6f29d
Jason Pitts <juniorjr083@gmail.com>
2018-06-16 04:39:42 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -14,31 +14,29 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
   hash = cart
-  
-  coupons.each do |ele|
-    item = ele[:item]
-   if !hash[:item] && hash[item][:count] >= ele[:num]
-     built_hash = {
-       "#{item} W/COUPON" =>{
-         :price => ele[:cost],
+  coupons.each do |coupon_hash|
+    # add coupon to cart
+    item = coupon_hash[:item]
+
+    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
         :clearance => hash[item][:clearance],
         :count => 1
-       }
-       
-     }
-     
-     if hash["#{item} W/COUPON"].nil?
-        hash.merge!(built_hash)
+        }
+      }
+      
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
       else
         hash["#{item} W/COUPON"][:count] += 1
         #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
       
-      hash[item][:count] -= ele[:num]
-   end 
-  end 
+      hash[item][:count] -= coupon_hash[:num]
+    end
+  end
   hash
 end
 
